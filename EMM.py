6b62280771c84b96b1a9e8705bf67dca216be628
Michael Luan <49159868+Large-Dog@users.noreply.github.com>
2023-01-16 13:37:45 -0500

    added code and report

---
+++ EMM.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 13 19:29:36 2022
+
+@author: micha
+"""
+import numpy as np  # for numerical python libraries
+
+import scipy.stats as st  # for statistical libraries
+
+import matplotlib.pyplot as plt  # for plotting
+
+import matplotlib.pylab as pylab
+
